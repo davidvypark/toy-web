9f5da1b21425b2a628ec0ccb7036c7d867c4ddfa
davidvypark <davidvypark@gmail.com>
2026-02-20 14:19:41 +0700

    redirect to app store link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/app',
+        destination: 'https://apps.apple.com/us/app/toy-group-video-cards/id6758913044',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
